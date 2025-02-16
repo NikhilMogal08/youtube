22c9cf3b3d70815bce4e846dc811c23e0e21108c
Nikhil Mogal <nikhilmogal01.sit.comp@gmail.com>
2025-02-16 05:23:57 +0000

    Conversion Operation

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -33,6 +33,7 @@
 // console.log(str3)
 
 // console.log("1"+2)
+
 // console.log(1+"2")
 // console.log("1"+2+2) //this will not perform addition of 2+2 it will give output 122 because in this print statement string "1" is first
 // console.log(1+2+"2") //this will first perfrom addition of 1+2 and then it will give output as 32 because string "2" is at last
