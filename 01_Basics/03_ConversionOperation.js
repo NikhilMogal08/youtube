8113b845149212c2e44c45798104e69b2bd218e4
Nikhil Mogal <nikhilmogal01.sit.comp@gmail.com>
2025-02-16 05:23:04 +0000

    conversion operation

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -11,12 +11,30 @@
 // "33abc" => NaN(Not a Number)
 // true => 1; false => 0;
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue)
+
+// let str1 = "hello"
+// let str2 = "nikhil"
+
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2) //this will not perform addition of 2+2 it will give output 122 because in this print statement string "1" is first
+// console.log(1+2+"2") //this will first perfrom addition of 1+2 and then it will give output as 32 because string "2" is at last
+
+// console.log(true)
